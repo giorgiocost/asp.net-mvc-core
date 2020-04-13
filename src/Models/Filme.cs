4a6c31150a9eaea19cdce96a8fc7da28fa07e2fb
Giorgio Costa <gegiorgio1991@gmail.com>
2020-04-13 02:12:51 -0300

    model filme

---
+++ src/Models/Filme.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace src.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+        public int Avaliacao { get; set; }
+    }
+}
